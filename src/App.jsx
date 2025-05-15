f143a2d6b19e6b5ca89981ba51c092eb2a7fbccb
Intelliod <intelliod@gmail.com>
2025-05-15 11:14:47 +0530

    updated with backend

--- src/App.jsx
+++ src/App.jsx
@@ -241,7 +241,7 @@ function App() {
                   path="/"
                   element={
                     <>
-                      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                      <div className="grid grid-cols-1 gap-6">
                         {paginatedJobs.map((job) => (
                           <Link key={job.id} to={`/job/${job.id}`} className="block">
                             <JobCard job={job} />
